cac0fcd05b0e68327924b9cb9ef058cd70c562c4
Alex Dzhikvas <skyborn8@gmail.com>
2024-01-24 15:42:24 +0300

    Add quote type switch

--- script.js
+++ script.js
@@ -11,12 +11,35 @@ iframes.set('V2 Sparrow', 'https://sparrow.staging.insurance-portal.com.au/v2/qu
 iframes.set('V1 Laneways.Agency', 'https://staging.keystoneonline.com.au/quote_financials/new?company_id=57')
 iframes.set('V2 Laneways.Agency', 'https://staging.keystoneonline.com.au/v2/quotes/financials/new?company_id=57')
 
-function switchIframe(event) {
-  const scriptUrl = event.target.value
+function replaceUrlQuoteType(url) {
+  const quoteType = window.quoteTypeSelect.value
 
+  switch (quoteType) {
+    case 'financial':
+      return url;
+      break;
+    case 'not_profit':
+      return url
+        .replace('quote_financials', 'quote_not_profits')
+        .replace('v2/quotes/financials', 'v2/quotes/not_profits')
+      break;
+    case 'jewellery':
+      return url
+        .replace('quote_financials', 'quote_jewelleries')
+        .replace('v2/quotes/financials', 'v2/quotes/jewelleries')
+      break;
+  }
+}
+
+function switchIframeOnRadioChange(event) {
+  return switchIframe(event.target.value)
+}
+
+function switchIframe(scriptUrl) {
   const script = document.createElement('script')
   script.src = 'https://staging.insurance-portal.com.au/reseller.js'
-  script.dataset.src = scriptUrl
+  script.dataset.src = replaceUrlQuoteType(scriptUrl)
+  console.log(script.dataset.src)
 
   window.iframeContainer.replaceChildren(script)
 }
@@ -29,6 +52,14 @@ for ([title, scriptUrl] of iframes) {
         <span class="peer-checked:text-blue-600 peer-checked:font-medium">${title}</span>
       </label>
     `
-  template.content.querySelector('input').addEventListener('change', switchIframe)
+  template.content.querySelector('input').addEventListener('change', switchIframeOnRadioChange)
   window.switchContainer.appendChild(template.content)
 }
+
+window.quoteTypeSelect.addEventListener('change', () => {
+  const checkedRadioElement = document.querySelector('input[type=radio]:checked')
+
+  if (checkedRadioElement) {
+    switchIframe(checkedRadioElement.value)
+  }
+})
